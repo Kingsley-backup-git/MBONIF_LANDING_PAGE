401ba1443ffa0d43a2ddba6d4d776d9ed3d549bb
Kingsley <okezekingsley18@gmail.com>
2026-05-29 05:55:39 +0100

    fixed marquee skipping

--- src/components/SectionEight.tsx
+++ src/components/SectionEight.tsx
@@ -1,7 +1,8 @@
 import ImgSample from "../assets/IMG_1695.jpeg";
 import DefaultMarqueeRow from "./defaultMarqueeRow";
 import { FiInstagram } from "react-icons/fi";
-
+import MarqueeImport from "react-fast-marquee";
+const Marquee = (MarqueeImport as any).default ?? MarqueeImport;
 function SectionEight() {
   const Images = [
     ImgSample,
@@ -13,24 +14,22 @@ function SectionEight() {
     ImgSample,
   ];
   return (
-    <div className="mt-1  h-[380px] ">
-      <DefaultMarqueeRow speed={80}>
-        {Images.map((item, index) => {
-          return (
-            <div
-              key={index}
-              className="border-white/50 max-w-[350px] h-full group border relative  w-full cursor-pointer overflow-hidden"
-            >
-              <img
-                src={item}
-                className=" group-hover:scale-115  transition-all duration-300 w-full h-full  object-cover "
-              />
-              <FiInstagram className="text-5xl opacity-0 group-hover:opacity-100 duration-300 ease-linear  text-white absolute m-auto inset-0" />
-            </div>
-          );
-        })}
-      </DefaultMarqueeRow>
-    </div>
+    <Marquee speed={80} className="mt-1">
+      <div className="flex  gap-1.5 h-[380px]">
+        {Images.map((item, index) => (
+          <div
+            key={index}
+            className="border-white/50 max-w-[350px] h-full group border relative w-full cursor-pointer overflow-hidden"
+          >
+            <img
+              src={item}
+              className="group-hover:scale-115 transition-all duration-300 w-full h-full object-cover"
+            />
+            <FiInstagram className="text-5xl opacity-0 group-hover:opacity-100 duration-300 ease-linear text-white absolute m-auto inset-0" />
+          </div>
+        ))}
+      </div>
+    </Marquee>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+
   plugins: [react(),  tailwindcss()],
+  optimizeDeps: {
+    include: ['react-fast-marquee'],
+  },
+  ssr: {
+    noExternal: ['react-fast-marquee'],
+  },
 })
